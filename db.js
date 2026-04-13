b626a8b53dfa88fc42b2e75b1a20cb1e81a2c5bb
riddhipatel18 <riddhipatel181931@gmail.com>
2026-04-13 17:00:14 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,19 +1,23 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect(err => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error("❌ DB Error:", err);
   } else {
     console.log("✅ Connected to Railway DB");
+    connection.release();
   }
 });
 
